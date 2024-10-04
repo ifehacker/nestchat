183aaa73e32e5ee913796c3e25e8b80e1d63cf17
ifeHacker <ife.oshoba@gmail.com>
2024-10-03 20:46:48 -0600

    implement frontend static

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,9 +1,41 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: any): void {
+    this.server.emit('msgToClient', payload);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 }
